dfeb40bf81268da1ae21263193d76a732d03261f
Natanael Arndt <arndtn@gmail.com>
2020-08-28 16:22:26 +0200

    Add intro.js tour

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,15 @@
   <div id="app" class="container-fluid">
     <div class="row">
     <div class="col-3">
-    <div v-intro="'The content of tooltip'">
-      <SparqlConnection/>
-    </div>
-    <GraphList/>
-    <ClassList/>
+    <SparqlConnection v-intro="'This is the configuration of the SPARQL Endpoint. Currently a <a href=\'http://github.com/AKSW/QuitStore\'>Quit Store</a> is used.'"/>
+    <GraphList v-intro="'This is the list of all named graphs in the connected store. With the plus-button a new graph can be created.'"/>
+    <ClassList v-intro="'This is the list of all classes in the selected graph.'"/>
     </div>
     <div class="col-9">
-      <ul class="nav nav-tabs">
+      <ul class="nav nav-tabs" v-intro="'This is the navigation list.'">
         <li class="nav-item"><router-link class="nav-link" active-class="active" to="/dashboard">Home</router-link></li>
         <li class="nav-item"><router-link class="nav-link" active-class="active" to="/instances">Instances</router-link></li>
-        <li class="nav-item"><router-link class="nav-link" active-class="active" to="/voc">Vocabulary</router-link></li>
+        <li class="nav-item" v-intro="'Please click on this tab.'"><router-link class="nav-link" active-class="active" to="/voc">Vocabulary</router-link></li>
         <li class="nav-item"><router-link class="nav-link" active-class="active" to="/edit">Edit</router-link></li>
         <li class="nav-item"><router-link class="nav-link" active-class="active" to="/rdform">RDForm</router-link></li>
         <li class="nav-item"><router-link class="nav-link" active-class="active" to="/add">Add</router-link></li>
@@ -37,8 +35,31 @@ export default {
     ClassList
   },
   mounted () {
-    this.$intro().start() // start the guide
-    this.$intro().showHints() // show hints
+    const h = this.$createElement
+    // Increment the toast count
+    // Create the message
+    const vNodesMsg = h(
+      'div',
+      {},
+      [
+        h('div', {}, 'Hi! If you want you can get a guided tour through OntoPad.'),
+        h('div', { style: { 'text-align': 'right' } }, [h('b-button', { on: { click: this.startTour } }, 'Start Tour')])
+      ]
+    )
+    this.$bvToast.toast([vNodesMsg], {
+      title: 'Guided Tour',
+      id: 'tourToast',
+      variant: 'info',
+      solid: true,
+      noAutoHide: true
+    })
+  },
+  methods: {
+    startTour () {
+      this.$bvToast.hide('tourToast')
+      this.$intro().start() // start the guide
+      this.$intro().showHints() // show hints
+    }
   }
 }
 </script>
