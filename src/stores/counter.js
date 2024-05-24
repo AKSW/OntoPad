b542a868ce0bf3fee0ba4964b3b8f96b3c015c46
Natanael Arndt <arndtn@gmail.com>
2024-05-24 12:10:06 +0200

    Fix counter store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,8 +4,13 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+  function increment(value) {
+    if (typeof value === 'undefined') {
+      count.value++;
+    } else {
+      count.value += value;
+    }
+
   }
 
   return { count, doubleCount, increment }
