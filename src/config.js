6b7f33e0ddfd6cefa219b3c7dfee80a91892433f
Natanael Arndt <arndtn@gmail.com>
2024-05-24 17:01:12 +0200

    set the config

--- src/config.js
+++ src/config.js
@@ -5,5 +5,13 @@ To use the config do `import config from '@/config'`.
 
 See also <https://medium.com/@marius.dras/dockerize-a-vue-app-with-a-configuration-file-a77cd98c879b>
 */
+let APP_CONFIG = {
+  "endpoint": {
+    "quit_url":"http://localhost:5000"
+  },
+  "graph_iri": "http://example.org/",
+  "resource_iri": "http://example.org/Resource"
+}
+
 // eslint-disable-next-line
 export default APP_CONFIG
