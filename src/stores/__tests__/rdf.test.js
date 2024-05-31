1cdf905c449cd610436b3427ed98765267964788
Natanael Arndt <arndtn@gmail.com>
2024-05-31 13:05:26 +0200

    Add rdf store test

---
+++ src/stores/__tests__/rdf.test.js
@@ -0,0 +1,122 @@
+// stores/counter.spec.ts
+import { describe, it, vi, expect, assert, beforeEach, afterEach } from 'vitest'
+
+import axios from 'axios';
+import { setActivePinia, createPinia } from 'pinia'
+import { useRdfStore } from '../rdf'
+
+const mocks = vi.hoisted(() => ({
+  get: vi.fn(),
+  post: vi.fn(),
+}));
+
+vi.mock('axios', async (importOriginal) => {
+  const actual = await importOriginal()
+  return {
+    default: {
+      ...actual.default,
+      create: vi.fn((args) => ({
+        ...actual.default.create(args),
+        get: mocks.get,
+        post: mocks.post,
+      })),
+      get: mocks.get,
+      post: mocks.post
+    }
+  }
+})
+
+describe('RDF Store', () => {
+  beforeEach(() => {
+    // creates a fresh pinia and makes it active
+    // so it's automatically picked up by any useStore() call
+    // without having to pass it to it: `useStore(pinia)`
+    setActivePinia(createPinia())
+  })
+
+  afterEach (() => {
+    vi.restoreAllMocks()
+  })
+
+  it('init', () => {
+    const store = useRdfStore()
+    expect(store.graph_iri).toBe("http://example.org/")
+    expect(store.resource_iri).toBe("http://example.org/Resource")
+    expect(store.sparqlEndpoint.quitUrl).toBe("http://localhost:5000")
+    expect(store.sparqlEndpoint.queryUrl).toBe("http://localhost:5000/sparql")
+    expect(store.sparqlEndpoint.updateUrl).toBe("http://localhost:5000/sparql")
+  })
+
+  it('send query', async () => {
+    // setup
+    //${store.sparqlEndpoint.queryUrl}
+    const postResponse = {
+     "config": {
+       "baseURL": "http://localhost:5000/sparql",
+       "data": "select * {?s ?p ?o}",
+       "headers": {
+         "Accept": "application/sparql-results+json",
+         "Content-Type": "application/sparql-query",
+       },
+       "method": "post",
+     },
+     "data": {
+       "head": {
+         "vars": [
+           "p",
+           "s",
+           "o",
+         ],
+       },
+       "results": {
+         "bindings": [],
+       },
+     },
+     "headers": {
+       "content-length": "64",
+       "content-type": "application/sparql-results+json",
+     },
+     "status": 200,
+     "statusText": "OK",
+    }
+
+    // mocks.post.mockImplementationOnce(() => Promise.resolve(postResponse))
+    mocks.post.mockResolvedValue(postResponse)
+
+    // execute
+    const store = useRdfStore()
+    const sparqlQuery = "select * {?s ?p ?o}"
+    let result = await store.sendQuery(sparqlQuery)
+
+    // assert
+    assert.deepInclude(result.data, postResponse.data)
+    assert.deepInclude(result.headers, postResponse.headers)
+
+    // expect(axios.post).toHaveBeenCalledWith(
+    //   "",
+    //   sparqlQuery,
+    //   {
+    //     "headers": {
+    //       "Accept": "application/sparql-results+json",
+    //       "Content-Type": "application/sparql-query",
+    //     },
+    //     "params": {},
+    //   }
+    // );
+  })
+
+  it('change graph', () => {
+    const store = useRdfStore()
+    store.changeGraphIri("http://graph.example.org/")
+    expect(store.graph_iri).toBe("http://graph.example.org/")
+    expect(store.resource_iri).toBe("http://example.org/Resource")
+  })
+
+  it('change resource', () => {
+    const store = useRdfStore()
+    store.changeResourceIri("http://example.org/Res")
+    expect(store.graph_iri).toBe("http://example.org/")
+    expect(store.resource_iri).toBe("http://example.org/Res")
+  })
+
+})
