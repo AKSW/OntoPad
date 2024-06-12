c922966aef6b57f576306bb4e3413f225c07eeda
Natanael Arndt <arndtn@gmail.com>
2024-06-12 23:00:18 +0200

    Only include buffer from node polyfills

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nodePolyfills(),
+    nodePolyfills({
+      include: ['buffer'],
+    }),
     vue(),
   ],
   resolve: {
