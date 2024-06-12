b17f276eda59cce4b7234285012c1e99dcb82c5b
Natanael Arndt <arndtn@gmail.com>
2024-06-12 23:40:33 +0200

    Set dashboard as default route

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: { path: "/dashboard" },
     },
     {
       path: '/about',
