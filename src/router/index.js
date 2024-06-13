15bbc66bec12af3535ab85226f35b11dd77ae0f0
Natanael Arndt <arndtn@gmail.com>
2024-06-13 23:06:04 +0200

    Remove hello wrld page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import { useRdfStore } from '../stores/rdf'
 
 const router = createRouter({
