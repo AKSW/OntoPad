ce79bab4201f5a9b3d86955af156a63715ed8b78
Norman Radtke <norman.radtke@gmail.com>
2024-06-27 17:59:03 +0200

    Add shacl-form to template of View

--- src/views/FormDemo.vue
+++ src/views/FormDemo.vue
@@ -1,10 +1,38 @@
 <template>
+  <div>
+    <dl>
+      <dt>IRI</dt>
+      <dd>{{ resource_iri }}</dd>
+      <dt>Label</dt>
+      <dd><Term v-model:term="label" @click="selectResource"/></dd>
+    </dl>
+  </div>
 
+  <!--
+      SHACL shapes can be defined on the attribute 'data-shapes'
+      or can be loaded by setting attribute 'data-shapes-url'
+    -->
+    <shacl-form data-shapes="
+      @prefix sh: <http://www.w3.org/ns/shacl#> .
+      @prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> .
+      @prefix ex: <http://example.org#> .
+
+      ex:ExampleShape
+        a sh:NodeShape, rdfs:Class ;
+        sh:property [
+          sh:name 'my value' ;
+          sh:path ex:exampleValue ;
+          sh:maxCount 3 ;
+        ] .
+    "></shacl-form>
 </template>
 
 <script>
 import { mapState } from 'pinia'
 import { useRdfStore } from '../stores/rdf'
+
+import { registerPlugin } from '@ulb-darmstadt/shacl-form'
+// import { LeafletPlugin } from '@ulb-darmstadt/shacl-form/plugins/leaflet.js'
 // import * as jsonld from 'jsonld'
 import rdf from '@rdfjs/data-model'
 
