280667cb76798be091ea8829e5e8509ce56f8757
Natanael Arndt <arndtn@gmail.com>
2024-08-04 12:59:58 +0200

    Use web hash history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useRdfStore } from '../stores/rdf'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
