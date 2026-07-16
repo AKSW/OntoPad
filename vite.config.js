7c82612ff13dd8488f7952b15f4a6df90a5fa9f9
Natanael Arndt <arndtn@gmail.com>
2026-07-16 17:44:53 +0200

    don't output index.html anymore, set name

--- vite.config.js
+++ vite.config.js
@@ -19,15 +19,12 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.js'),
+      name: 'ontopad',
       fileName: 'ontopad',
       formats: ['es', 'cjs']
     },
     rollupOptions: {
       external: ['vue', 'pinia'],
-      input: {
-        main: resolve(import.meta.dirname, 'src/main.js'),
-        index: resolve(import.meta.dirname, 'index.html'),
-      },
       output: {
         exports: 'named',
         globals: {
